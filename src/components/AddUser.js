d2b306963e815910bbc7b497831dab6d4de789ec
Sophia Mai <phuongmai001@gmail.com>
2018-12-03 21:15:26 -0800

    add AddUser state, handleInputChange method, and handleSubmit method

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 
 export default class AddUser extends Component {
+  state = {
+    user: {
+      firstName: "",
+      lastName: "",
+      username: ""
+    },
+    userExist: false
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    // take the original object in state, update the user's fields accoridingly
+    // where firstName: value, lastName: value (of lastName input), & username: value(of username input)
+    this.setState((currentState) => ({
+      ...currentState,
+      user: {
+        ...currentState.user,
+        [name]: value
+      }
+    }));
+  }
+
+  handleSubmit = () => {
+    // 
+  }
+
   render() {
     return (
       <div>
