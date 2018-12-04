b6200f220462e74a03834d7b4ad233d55f017abf
Sophia Mai <phuongmai001@gmail.com>
2018-12-03 20:14:36 -0800

    feature: enable UserList component to display list of Users and toggle show/hide #gamesPlayed

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,11 +3,41 @@ import React, { Component } from 'react';
 import User from './User';
 
 export default class UserList extends Component {
+  state = {
+    showGamesPlayed: false // initialize condition for showing # of games played
+  }
+
+  toggleGamesPlayedPanel = () => {
+    this.setState({ showGamesPlayed: true });
+  }
+
   render() {
+    const { showGamesPlayed } = this.state;
+    const users = this.props;
+
+    const gamesPlayedButton = (
+      <div>
+        <button onClick={this.toggleGamesPlayedPanel}>
+          {showGamesPlayed ? "Show" : "Hide"}
+          the number of games played
+        </button>
+      </div>
+    );
+
     return (
       <div>
         <h1> Hello from UserList component </h1>
-        <User />
+        {users && users.length > 0 ? gamesPlayedButton : ""}
+
+        <ol>
+          {users.length > 0 ? users.map(user => (
+            <User key={user.username}
+              user={user}
+              showGamesPlayed={showGamesPlayed}
+            />
+          )) : (<p>Add new user using the above form</p>)}
+        </ol>
+
       </div>
     );
   }
