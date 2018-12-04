7caa2e115b7e8dd073af0ad33ffec3f0c2b32efa
Sophia Mai <phuongmai001@gmail.com>
2018-12-03 21:15:53 -0800

    add AddUser render method for three input fields

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -29,9 +29,33 @@ export default class AddUser extends Component {
   }
 
   render() {
+    const { firstName, lastName, username } = this.state.user;
+
     return (
       <div>
-        <h1> Hello from AddUser component </h1>
+        <h1>New User Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input type="text"
+              name="firstName"
+              placeholder="Enter first name:"
+              value={firstName}
+              onChange={this.handleInputChange}
+            />
+            <input type="text"
+              name="lastName"
+              placeholder="Enter last name:"
+              value={lastName}
+              onChange={this.handleInputChange}
+            />
+            <input type="text"
+              name="username"
+              placeholder="Enter username:"
+              value={username}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
