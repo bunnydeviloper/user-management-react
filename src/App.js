f45b01fa50c88d33160773a084683cc9e63d04e1
Sophia Mai <phuongmai001@gmail.com>
2018-12-03 19:42:47 -0800

    remove initial create-react-app page and add two new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import AddUser from './components/AddUser';
+import UserList from './components/UserList';
+
 class App extends Component {
+  state = {
+    users: {}
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AddUser users={this.state.users} onAddUser={this.state.addNewUser} />
+        <UserList users={this.state.users} />
       </div>
     );
   }
