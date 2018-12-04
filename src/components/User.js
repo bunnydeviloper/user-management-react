0c075b68cddd08e77b63654c8cb383b577236a39
Sophia Mai <phuongmai001@gmail.com>
2018-12-03 19:45:33 -0800

    create User component

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <h3> Hello from User component </h3>
+      </div>
+    );
+  }
+}
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import User from './User';
+
 export default class UserList extends Component {
   render() {
     return (
       <div>
         <h1> Hello from UserList component </h1>
+        <User />
       </div>
     );
   }
